faf59d9a5e96dd1bea28aa67309bb5670ea1ad0e
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2021-08-12 22:18:02 -0300

    update medicine

--- src/utils/medicineValidation.ts
+++ src/utils/medicineValidation.ts
@@ -9,9 +9,29 @@ export async function medicineCreateValidation(
   const schema = Yup.object().shape({
     idRegister: Yup.string().required(),
     nome: Yup.string().required(),
-    categoria: Yup.boolean().required(),
+    categoria: Yup.string().required(),
     classe_terapeutica: Yup.string().required(),
     empresa_detentora: Yup.string().required(),
+    dosagem: Yup.string().required(),
+  });
+
+  try {
+    await schema.validate(objectMedicine, { abortEarly: false });
+  } catch (error) {
+    throw new ValidationError(error);
+  }
+}
+
+export async function medicineUpdateValidation(
+  objectMedicine: IMedicine,
+): Promise<void> {
+  const schema = Yup.object().shape({
+    idRegister: Yup.string().required(),
+    nome: Yup.string().required(),
+    categoria: Yup.string().required(),
+    classe_terapeutica: Yup.string().required(),
+    empresa_detentora: Yup.string().required(),
+    dosagem: Yup.string().required(),
   });
 
   try {
